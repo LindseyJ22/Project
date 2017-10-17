5e1af6a183438524b6aa355da40b521f5acfbf77
Lindsey Johnson <redhook@hotmail.com>
2017-10-16 20:14:52 -0600

    added list pokemon and createsprite

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-$(function(){
-
-
-
 class Pokedex {
 	constructor(pokemonArray){
 		this.pokemonArray = pokemonArray;
 	}
 
+	showDetails(){
+
+	}
+
+	listImages(){
+		for (var i = 0; i < pokemonArray.length; i++) {
+			cachedFetch(`https://pokeapi.co/api/v2/pokemon/${pokemonArray[i]}`)
+			.then(r => r.json()) 
+			.then(res => { 
+			
+			console.log(res);
+			let pokePicture = res;
+				$('#image').append(`<img src="${pokePicture.sprites.front_default}" />`);
+		 	});
+		}
+	}
+
+	getSprite(id){
+		cachedFetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+		.then(r => r.json()) 
+		.then(res => { 
+			console.log(res);
+			$('#image').html('');
+			$('#image').append(`
+				<img src="${res.sprites.front_default}" />
+			`);
+		 })
+			
+	}
+
 	
+
+	listAllPokemon(){
+		for (var i = 0; i < pokemonArray.length; i++) {
+			$('#names').append(`<div class="pokeName" data-count="${i}"> ${pokemonArray[i]} </div>`);
+		}
+	}
+
+	sortPokemon(){
+
+	}
+
 	cacheCall(url, func){
 		cachedFetch(url) 
 		.then(r => r.json()) 
-		.then(res => { func() })
+		.then(res => {  })
 	}
 }
 
-console.log(pokemonArray);
 
-let testDex = new Pokedex();
 
-testDex.
+$(function(){ // Document Ready Function
 
-});
+	let pokemon = new Pokedex();
+
+	$(document).on('click', '.pokeName', function() {
+		let id = $(this).data('count');
+		pokemon.getSprite(id + 1);
+	});
+
+	pokemon.listAllPokemon();
+	pokemon.cacheCall('https://pokeapi.co/api/v2/pokemon/')
+
+
+
+}); // End of document ready function
