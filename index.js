8dda07b8d9d14dd2e7d3c3d301c5b436aab9170e
Lindsey Johnson <redhook@hotmail.com>
2017-10-17 16:39:43 -0600

    added a new method that displays the favorites list

--- index.js
+++ index.js
@@ -1,57 +1,55 @@
-$(function(){
+
 class Pokedex {
 	constructor(pokemonArray){
-		this.pokemonArray = pokemonArray;
+		this.pokemonArray = [];
+		this.favoriteArray = [];
+		this.currentPokemon = null;
 	}
 
-
-
-
-	getSprite(id){
+	getPokemonDetails(id){
 		cachedFetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 		.then(r => r.json()) 
 		.then(res => { 
 			console.log(res);
+				
 			
-			$('#image').html('');
-			$('#name').html('');
-			$('#weight').html('');
-			$('#poke_id').html('');
-			$('#height').html('');
-			$('#types').html('');
-			$('#image').append(`<img src="${res.sprites.front_default}" />`); 
-			$('#name').append("Name: " + res.name);
-			$('#weight').append("Weight: " + res.weight);
-			$('#poke_id').append("ID: " + res.id);
-			$('#height').append("Height: " + res.height);
+			$('#image').html(`<img src="${res.sprites.front_default}" />`); 
+			$('#name').html("Name: " + res.name);
+			$('#weight').html("Weight: " + res.weight);
+			$('#poke_id').html("ID: " + res.id);
+			$('#height').html("Height: " + res.height);
 			$('#types').html("Type: ");
 			for (var i = 0; i < res.types.length; i++){
 				$('#types').append("<li>" + res.types[i].type.name + "</li>")
 			}//ends for loop
-			
-		 });//ends cacheFetch
+			this.currentPokemon = res.name;
+		});//ends cacheFetch
 			
 	}//ends get sprite
 
-		addFavorite(id){
-			cachedFetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-			.then(r => r.json()) 
-			.then(res => { 
-			// console.log(res);
-			$(document).on('click', '#favorite', function(){
-				$('#favorite_container').html('');
-				$('#favorite_container').append("<li> " + res.name + "</li>");
-		
-			});//ends click function
-		});
+	addFavorite(){
+		if (this.favoriteArray.includes(this.currentPokemon)){
+			 this.favoriteArray.splice(this.favoriteArray.indexOf(this.currentPokemon), 1);
+		}else{
+			this.favoriteArray.push(this.currentPokemon);
 		}
-	
+		this.displayFavorites();
+			// console.log(res);			
+	}//ends addFavorite
+	displayFavorites(){
+		var html = '';
+		for (var i = 0; i < this.favoriteArray.length; i++) {
+			html += "<li>" + this.favoriteArray[i] + "</li>";
+		}
+		$('#favorite_container').html(html);
 
+	}
+	
 	listAllPokemon(){
 		for (var i = 0; i < pokemonArray.length; i++) {
-			$('#names').append(`<div class="pokeName" data-count="${i}"> ${pokemonArray[i]} </div>`);
+		$('#names').append(`<div class="pokeName" data-count="${i}"> ${pokemonArray[i]} </div>`);
 		}
-	}
+	}//ends listallpokemon
 
 	
 
@@ -59,20 +57,20 @@ class Pokedex {
 }//ends pokedex
 
 
-
+$(function(){
  // Document Ready Function
 
 	let pokemon = new Pokedex();
 
 	$(document).on('click', '.pokeName', function() {
 		let id = $(this).data('count');
-		pokemon.getSprite(id + 1);
-		pokemon.addFavorite(id + 1);
+		pokemon.getPokemonDetails(id + 1);
 	});
 	
+	$(document).on('click', '#favorite', function(){
+		pokemon.addFavorite();
+	});
 
 	pokemon.listAllPokemon();
-
-
 
 }); // Ends document readhy
